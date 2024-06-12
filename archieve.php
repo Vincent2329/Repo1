8ed789bb789c158daf4770b5a9f6d5477a4279c1
Hermoso1172 <ic.clarenceherold.hermoso@cvsu.edu.ph>
2024-06-12 14:45:25 +0800

    Add files via upload

---
+++ archieve.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img src="" alt="">
+
+
+    </header>
+    <nav>
+            <div class="profile">
+                    <img src="" alt=""></img>
+                    <p>Admin</p>
+                </div>
+            </div>
+           
+            <div class="container"> </div>
+
+                <div class="menu_list">
+                    <a href=""><button type="button" >APPLICATION</button></a>
+                    <a href=""><button type="button" >EMPLOYEES</button></a>
+                    <a href=""><button type="button" 
+                    style="border-bottom-right-radius:50px;">ARCHIVE</button></a>
+
+                    <a href=""><button type="button" 
+                    style="
+                    background-color:white;
+                    color: black;
+                    border-top-left-radius:50px;
+                    border-bottom-left-radius:50px;"
+                    >PAYROLL
+                    </button></a>
+
+                    <a href=""><button type="button" 
+                    style="border-top-right-radius:50px;">LOG-OUT</button></a>
+                </div>
+    </nav>
+    <article>
+        <?php
+            echo "hello";
+        ?>
+     </article>
+</body>
+</html>
