4e2b87957213b7e830b5bfbbd919ba204e0daa00
Vincent2329 <asuncionjervin06@gmail.com>
2024-06-30 14:08:38 +0800

    Add files via upload

---
+++ ADMIN_employee.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="newsample.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-sticky-top" style="background-color: #D9D9D9;">
+        <div class="container-fluid">
+            <a class="navbar-brand fs-2" href="ADMIN_applicant.php">
+                <img src="paycor.png" height="40" width="70" class="d-inline-block align-text-center">
+                Paycor
+            </a>
+        </div>
+    </nav>
+    </nav>
+
+    <!--Side bar-->
+    <div class="container-fluid bg-light">
+        <div class="row cols-2">
+            <nav
+                class="col-sm-2 flex-column justify-content-center justify-content-evenly text-center sidebar rounded rounded-end p-lg-3 h-100 w-">
+                <div class="nav-item" style="background-color: #405654;">
+                    <img src="profile.png" alt="Profile Picture" class="align-self-center" height="100" width="100">
+                    <h1 class="h3 fw-bolder">Admin</h1>
+                    <p class="text-info">Online</p>
+                </div>
+                <div class="nav-item">
+                    <a href="ADMIN_applicant.php" class="h5 text-decoration-none">APPLICANT</a>
+                </div>
+                <div class="nav-item bg-body-tertiary rounded rounded-start-pill rounded-top-5 w-100 text-black ms-3 align-content-start">
+                    <a href="#" class="h5 text-decoration-none">EMPLOYEE</a>
+                </div>
+                <div class="nav-item">
+                    <a href="#" class="h5 text-decoration-none">ARCHIVE</a>
+                </div>
+                <div class="nav-item">
+                    <a href="#" class="h5 text-decoration-none">PAYROLL</a>
+                </div>
+                <div class="nav-item">
+                    <a href="#" class="h5 text-decoration-none">LOGOUT</a>
+                </div>
+            </nav>
+            <div class="col-sm-2">
+                <select class="form select">
+                    <option>IT Technician</option>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                </select>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
